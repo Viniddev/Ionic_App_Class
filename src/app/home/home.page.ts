3899d3e0b1795393d37a1086d30d1fd77b534d54
Vinicius <diasvinicius95@outlook.com>
2025-02-20 22:18:08 -0300

    feat: design pra aula 01

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { logoIonic, airplaneOutline } from 'ionicons/icons';
+import { IonIcon } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon],
 })
+
 export class HomePage {
-  constructor() {}
+  constructor() {
+    addIcons({ logoIonic, airplaneOutline });
+  }
 }
