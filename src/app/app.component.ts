3c51409c5df7f06673012bec6b606eeb7c6281ff
Vinicius Dias <diasvinicius95@outlook.com>
2025-05-14 20:26:23 -0300

    feat: correcoes layout pra ficar compativel com o mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,6 @@ import { IonApp, IonRouterOutlet} from '@ionic/angular/standalone';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['./app.component.scss'],
   imports: [IonApp, IonRouterOutlet],
   standalone: true,
 })
